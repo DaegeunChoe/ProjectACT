fb42edf33dd77304cac0d34d24e2473969a56e73
Daegeun Choe <asdf12gg@gmail.com>
2026-02-02 13:54:49 +0900

    pawn data에 입력, GAS 관련 멤버변수 추가

--- Source/ProjectACT/Character/ActPawnData.cpp
+++ Source/ProjectACT/Character/ActPawnData.cpp
@@ -1,2 +1,5 @@
 #include "Character/ActPawnData.h"
 
+UActPawnData::UActPawnData()
+{
+}
--- Source/ProjectACT/Character/ActPawnData.h
+++ Source/ProjectACT/Character/ActPawnData.h
@@ -3,9 +3,20 @@
 #include "Engine/DataAsset.h"
 #include "ActPawnData.generated.h"
 
-UCLASS()
+class UActAbilitySet;
+class UActInputConfig;
+
+UCLASS(BlueprintType, Const)
 class PROJECTACT_API UActPawnData : public UDataAsset
 {
 	GENERATED_BODY()
 	
+public:
+	UActPawnData();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Act")
+	TArray<TObjectPtr<UActAbilitySet>> AbilitySets;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Act")
+	TObjectPtr<UActInputConfig> InputConfig;
 };
